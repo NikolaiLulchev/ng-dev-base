c72ef8e9ac8569b6c290746a4a9a63af683a5f01
NikolaiLulchev <nikolai.lulchev@gmail.com>
2022-12-08 17:18:59 +0200

    Initial commit

---
+++ src/app/materil/material.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+
+const MaterialComponents = [
+  MatSlideToggleModule
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule {
+}
