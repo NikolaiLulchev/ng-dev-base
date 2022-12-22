3b83fe311c09813697aecd8195d43daaa7791606
NikolaiLulchev <nikolai.lulchev@gmail.com>
2022-12-22 13:32:55 +0200

    Offers pagination works

--- src/app/pages/offers/offers.component.ts
+++ src/app/pages/offers/offers.component.ts
@@ -2,38 +2,30 @@ import {Component, OnInit} from '@angular/core';
 import {PositionEnum} from "../../core/enums/position.enum";
 import {LocationEnum} from "../../core/enums/location.enum";
 import {OfferDTO, OfferService} from "../../core/offer.service";
-import {PageEvent} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
   styleUrls: ['./offers.component.css']
 })
-export class OffersComponent implements OnInit{
-  position: PositionEnum;
+export class OffersComponent implements OnInit {
+  positions: PositionEnum;
   title: string;
-  location: LocationEnum;
+  locations: LocationEnum;
   company: string;
   addedOn: Date;
-  jobs: OfferDTO[] = [];
+  jobs: OfferDTO[];
+
   pageSize = 6;
-  pageEvent: PageEvent = {} as PageEvent;  // initialize as an empty object
+  pageIndex = 0;
 
-  displayedJobs: OfferDTO[];
-
-  updateDisplayedJobs() {
-    const startIndex = this.pageEvent.pageIndex * this.pageEvent.pageSize;
-    this.displayedJobs = this.jobs.slice(startIndex, startIndex + this.pageEvent.pageSize);
-  }
-
-  constructor(private offerService:OfferService) {
+  constructor(private offerService: OfferService) {
   }
 
   ngOnInit(): void {
     this.offerService.getAllOffers$().subscribe({
-      next:(jobs => this.jobs=jobs)
-    })
-    this.updateDisplayedJobs();
+      next: (jobs) => this.jobs = jobs,
+    });
   }
 }
 
