427863fdef1b5d338511fda504ae409efaf22a58
NikolaiLulchev <nikolai.lulchev@gmail.com>
2023-08-30 19:56:18 +0300

    delete unused

--- src/app/core/enums/gender.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.GenderEnum = void 0;
-var GenderEnum;
-(function (GenderEnum) {
-    GenderEnum["MALE"] = "Male";
-    GenderEnum["FEMALE"] = "Female";
-})(GenderEnum = exports.GenderEnum || (exports.GenderEnum = {}));
